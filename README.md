ff398026234869178aa2e54a03fe7a5fe9ac1394
Siddarth <pullakhandam.siddartha@gmail.com>
2024-06-22 18:39:23 -0400

    commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Podcast_summarizer_Agents
  Summarizing a whole podcast form youtube and send it as a slack message to a channel using crewai and composio.
+
+
+# setup(without docker):-
+
+##### clone the repository
+```bash
+ git clone https://github.com/siddartha-10/Podcast_summarizer_Agents.git
+```
+
+##### Install the packages
+```bash
+pip install -r requirements.txt
+```
+
+##### connect composio with slack and create an app
+```bash
+https://docs.composio.dev/apps/slack :- follow this documentation to connect slack and create an app.
+```
+
+##### Time to spin up the application
+```bash
+streamlit run main.py
+```
+
+# setup(with docker):-
+
+```bash
+download docker and spin up the docker daemon
+docker build 
+```
+
+2) crewai :- https://www.crewai.com/
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from Podcast_Summarizer_AI_Agent import PodSumCrew
+import streamlit as st
+
+st.set_page_config(page_title="Podcast Summary App", layout="centered")
 
 def run():
-    youtube_url = input("Please enter a Youtube Podcast URL: "),
-    slack_channel = input("Please enter the name of the Slack channel where you would like the agent to send the message: "),
-    inputs = {"youtube_url": youtube_url, "slack_channel": slack_channel}
-    result = PodSumCrew().crew().kickoff(inputs=inputs)
+    st.title("Podcast Summary App")
 
-if __name__ == '__main__':
+    podcast_url = st.text_input("Enter the URL of the Podcast you want to summarize")
+    slack_channel = st.text_input("Enter the Slack channel name")
+
+    # Run the summarization code whenever podcast_url changes
+    if st.button("Summarize Podcast"):
+        if podcast_url and slack_channel:
+            inputs = {'youtube_url': podcast_url, "slack_channel": slack_channel}
+            result = PodSumCrew().crew().kickoff(inputs=inputs)
+            st.write(result)
+        else:
+            st.write("podcast_url or slack channel is empty")
+              # Clear result if podcast_url is empty
+
+if __name__ == "__main__":
     run()
