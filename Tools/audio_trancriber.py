06a2c27940b418225eac3f58a1be4174ac9ebcdf
Siddarth <pullakhandam.siddartha@gmail.com>
2024-06-22 19:51:21 -0400

    Update audio_trancriber.py

--- Tools/audio_trancriber.py
+++ Tools/audio_trancriber.py
@@ -1,17 +1,17 @@
 from crewai_tools import tool
 from pytube import YouTube
 import whisper
-@tool("Audio Transribe tool and summary tool")
+@tool("Audio Transribe tool")
 def audio_transcriber_tool(url):
     """
-    Extracts audio and transcribe the audio from a YouTube video given its URL.
+    Extracts audio and transcribe the audio from a YouTube video given its URL and summarizes it.
 
     Parameters:
     - url (str): The URL of the YouTube video from which audio will be extracted.
 
     Returns:
     str: A string containing:
-        - Text from the YouTube URL
+        - The summarized version of the Transcribed Youtube URL
     """
     yt = YouTube(url)
     video = yt.streams.filter(only_audio=True).first()
